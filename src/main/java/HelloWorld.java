d996b7f106296929dada435d789914d49889c55d
saigit1 <82562086+saigit1@users.noreply.github.com>
2021-05-03 23:34:30 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World23");
+		get("/hello", (req, res) -> "Hello Worl");
 	}
 }
 
